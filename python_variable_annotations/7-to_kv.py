1f6e84bdcfb69db62abef0526b7e2747c552aea7
Cristian Acevedo <acecristian35@gmail.com>
2026-04-06 10:55:39 -0400

    p7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module for to_kv function."""
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple of string k and the square of v."""
+    return (k, float(v ** 2))
