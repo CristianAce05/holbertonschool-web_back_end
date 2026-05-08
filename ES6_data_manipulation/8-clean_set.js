46a3598b66b43891012e9fd5d675e9d24fe7344b
Cristian Acevedo <acecristian35@gmail.com>
2026-05-07 23:31:06 -0400

    p8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+  if (!startString || startString === '') {
+    return '';
+  }
+  
+  const result = [];
+  for (const value of set) {
+    if (value && value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  }
+  
+  return result.join('-');
+}
