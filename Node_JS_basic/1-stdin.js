c7b1ed33bb1f0196788cc1b66fb70799be040e24
Cristian Acevedo <acecristian35@gmail.com>
2026-05-11 11:31:47 -0400

    p1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    const name = chunk.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+  }
 });
 
-if (!process.stdin.isTTY) {
-  process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
